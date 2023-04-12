a77236ae73f5e3e18ada42fffa9e748055ee5e62
ctpage95 <ctpage95@gmail.com>
2023-04-12 19:28:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,55 +23,21 @@ resource "random_integer" "deployment_id_suffix" {
 
 // Resource Group
 
-resource "azurerm_resource_group" "rg2" {
-  name     = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-rg2"
+resource "azurerm_resource_group" "rgcam" {
+  name     = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-rgcam"
   location = var.location
 
   tags = local.tags
 }
 
-
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
   name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
-  resource_group_name      = azurerm_resource_group.rg2.name
-  location                 = azurerm_resource_group.rg2.location
+  resource_group_name      = azurerm_resource_group.rgcam.name
+  location                 = azurerm_resource_group.rgcam.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = local.tags
 }
-
-// Machine Learning Workspace
-
-provider "azurerm" {
-  features {}
-}
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_application_insights" "rg2" {
-  name                = "workspace-rg2-ai"
-  location            = azurerm_resource_group.rg2.location
-  resource_group_name = azurerm_resource_group.rg2.name
-  application_type    = "web"
-}
-
-resource "azurerm_key_vault" "rg2" {
-  name                = "workspaceexamplekeyvault"
-  location            = azurerm_resource_group.rg2.location
-  resource_group_name = azurerm_resource_group.rg2.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "premium"
-}
-
-
-
-resource "azurerm_machine_learning_workspace" "rg2" {
-  name                    = "rg2-workspace"
-  location                = azurerm_resource_group.rg2.location
-  resource_group_name     = azurerm_resource_group.rg2.name
-  application_insights_id = azurerm_application_insights.rg2.id
-  key_vault_id            = azurerm_key_vault.rg2.id
-  storage_account_id      = azurerm_storage_account.rg2.id
