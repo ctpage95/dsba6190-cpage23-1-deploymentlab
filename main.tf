4cda5af557e34fc634ea59fe3dbf1faa399767a1
ctpage95 <ctpage95@gmail.com>
2023-04-12 19:56:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -116,48 +116,11 @@ resource "azurerm_cosmosdb_account" "camdb" {
 
 }
 
-// Firewall
-
-resource "azurerm_virtual_network" "rgcam" {
-  name                = "testvnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rgcam.location
-  resource_group_name = azurerm_resource_group.rgcam.name
-}
-
-resource "azurerm_subnet" "rgcam" {
-  name                 = "AzureFirewallSubnet"
-  resource_group_name  = azurerm_resource_group.rgcam.name
-  virtual_network_name = azurerm_virtual_network.rgcam.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "rgcam" {
-  name                = "testpip"
-  location            = azurerm_resource_group.rgcam.location
-  resource_group_name = azurerm_resource_group.rgcam.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_firewall" "rgcam" {
-  name                = "testfirewall"
-  location            = azurerm_resource_group.rgcam.location
-  resource_group_name = azurerm_resource_group.rgcam.name
-  sku_name            = "AZFW_VNet"
-  sku_tier            = "Standard"
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.rgcam.id
-    public_ip_address_id = azurerm_public_ip.rgcam.id
-  }
-}
 
 // Data Factory
 
-//resource "azurerm_data_factory" "rgcam" {
-//  name                = "rgcam"
-//  location            = azurerm_resource_group.rgcam.location
-//  resource_group_name = azurerm_resource_group.rgcam.name
-//}
+resource "azurerm_data_factory" "rgcam" {
+  name                = "rgcam"
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+}
