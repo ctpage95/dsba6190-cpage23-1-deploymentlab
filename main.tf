b9cb973f2b179f12998376a95957742481c6cf96
ctpage95 <ctpage95@gmail.com>
2023-04-12 19:54:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,6 +38,126 @@ resource "azurerm_storage_account" "storage" {
   location                 = azurerm_resource_group.rgcam.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
   tags = local.tags
+  is_hns_enabled = true
+}
+
+// Machine Learning Workspace
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_application_insights" "rgcam" {
+  name                = "workspace-rgcam-ai"
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "rgcam" {
+  name                = "workspaceexamplekeyvault"
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+}
+
+
+resource "azurerm_machine_learning_workspace" "rgcam" {
+  name                    = "rgcam-workspace"
+  location                = azurerm_resource_group.rgcam.location
+  resource_group_name     = azurerm_resource_group.rgcam.name
+  application_insights_id = azurerm_application_insights.rgcam.id
+  key_vault_id            = azurerm_key_vault.rgcam.id
+  storage_account_id      = azurerm_storage_account.storage.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+
+// Cosmos
+
+resource "azurerm_cosmosdb_account" "camdb" {
+  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}camdb"
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "eastus"
+    failover_priority = 0
+  }
+
+}
+
+// Firewall
+
+resource "azurerm_virtual_network" "rgcam" {
+  name                = "testvnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+}
+
+resource "azurerm_subnet" "rgcam" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.rgcam.name
+  virtual_network_name = azurerm_virtual_network.rgcam.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "rgcam" {
+  name                = "testpip"
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "rgcam" {
+  name                = "testfirewall"
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.rgcam.id
+    public_ip_address_id = azurerm_public_ip.rgcam.id
+  }
+}
+
+// Data Factory
+
+resource "azurerm_data_factory" "rgcam" {
+  name                = "rgcam"
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
 }
