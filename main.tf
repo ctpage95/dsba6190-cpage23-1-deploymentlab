bec6c80376d2dd74b88c4403d7f87423186c58f6
ctpage95 <ctpage95@gmail.com>
2023-04-12 19:08:30 -0400

    added ml workspace

--- main.tf
+++ main.tf
@@ -42,3 +42,12 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
+// Machine Learning Workspace
+resource "azurerm_machine_learning_workspace" "camws" {
+  name                    = "camws-workspace"
+  location                = azurerm_resource_group.camws.location
+  resource_group_name     = azurerm_resource_group.camws.name
+  application_insights_id = azurerm_application_insights.camws.id
+  key_vault_id            = azurerm_key_vault.camws.id
+  storage_account_id      = azurerm_storage_account.camws.id
