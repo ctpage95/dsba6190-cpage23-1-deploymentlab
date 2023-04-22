451e8053ae77655bcd8231a66c51afffc35a568e
ctpage95 <ctpage95@gmail.com>
2023-04-22 14:51:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,40 +42,6 @@ resource "azurerm_storage_account" "storage" {
   is_hns_enabled           = true
 }
 
-// Machine Learning Workspace
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_application_insights" "appinscam" {
-  name                = "workspace-rgcam-ai"
-  location            = azurerm_resource_group.rgcam.location
-  resource_group_name = azurerm_resource_group.rgcam.name
-  application_type    = "web"
-}
-
-resource "azurerm_key_vault" "kvcam" {
-  name                = "workspaceexamplekeyvault"
-  location            = azurerm_resource_group.rgcam.location
-  resource_group_name = azurerm_resource_group.rgcam.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "premium"
-}
-
-
-resource "azurerm_machine_learning_workspace" "mlwscam" {
-  name                    = "rgcam-workspace"
-  location                = azurerm_resource_group.rgcam.location
-  resource_group_name     = azurerm_resource_group.rgcam.name
-  application_insights_id = azurerm_application_insights.appinscam.id
-  key_vault_id            = azurerm_key_vault.kvcam.id
-  storage_account_id      = azurerm_storage_account.storage.id
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-
 // Cosmos
 
 resource "azurerm_cosmosdb_account" "camdb" {
