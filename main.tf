6e6e6993d55bb54f9305badb8871c36417b26c47
ctpage95 <ctpage95@gmail.com>
2023-04-22 17:19:24 -0400

    Update main.tf
    changed df name and added firewall

--- main.tf
+++ main.tf
@@ -129,9 +129,48 @@ resource "azurerm_machine_learning_workspace" "mlwscam" {
 // Data Factory
 
 resource "azurerm_data_factory" "datafactorycam" {
-  name                = "df-dsba6190-cam6190"
+  name                = "df-dsba6190-cam6190-2"
   location            = azurerm_resource_group.rgcam.location
   resource_group_name = azurerm_resource_group.rgcam.name
+}
+
+
+// Firewall
+
+resource "azurerm_virtual_network" "vnetcam" {
+  name                = "testvnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+}
+
+resource "azurerm_subnet" "subnetcam" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.rgcam.name
+  virtual_network_name = azurerm_virtual_network.vnetcam.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pubipcam" {
+  name                = "testpip"
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "firewallcam" {
+  name                = "testfirewall"
+  location            = azurerm_resource_group.rgcam.location
+  resource_group_name = azurerm_resource_group.rgcam.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.subnetcam.id
+    public_ip_address_id = azurerm_public_ip.pubipcam.id
+  }
 }
 
 
